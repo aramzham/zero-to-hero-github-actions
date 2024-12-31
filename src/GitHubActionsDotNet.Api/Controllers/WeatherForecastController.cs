0f6170d57d85599d1ff5f0d6abe20c441d306ec0
Aram Zhamkochyan <aramzham@hotmail.com>
2025-01-01 03:08:26 +0400

    fix formatting

--- src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
+++ src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 namespace GitHubActionsDotNet.Api.Controllers;
+
 using GitHubActionsDotNet.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-                        private static readonly string[] Summaries =
+    private static readonly string[] Summaries =
     [
         "Freezing",
         "Bracing",
@@ -22,10 +23,10 @@ public class WeatherForecastController : ControllerBase
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get() => Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    {
-        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
-        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    })
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
         .ToArray();
 }
